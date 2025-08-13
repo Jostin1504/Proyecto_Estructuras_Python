1225fe052da5f3fc41502998ecf6d95bec3a6f3b
Elcaya4 <gjmadrigalc@gmail.com>
2025-08-12 19:49:07 -0600

    Commit v2.0

--- prueba.py
+++ prueba.py
@@ -1,6 +1,11 @@
+
 def mensaje():
     print("¡Hola, mundo Siuu!")
-
 def otro_mensaje():
-    print("¡Adiós, mundo Siuu!")
-    
+     print("¡Adiós, mundo Siuu!")
+def mensaje_nuevo():
+     print("Abra github")
+
+mensaje()
+otro_mensaje()
+mensaje_nuevo()
