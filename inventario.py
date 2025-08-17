14c73e0890c190ef7c50b5c775452097efa0b3a6
Waloe <54786573+Eibon-bot@users.noreply.github.com>
2025-08-17 13:20:51 -0600

    ListaDobleRevision

--- inventario.py
+++ inventario.py
@@ -9,22 +9,33 @@ class Nodo:
 class ListaInventario:
     def __init__(self):
         self.primero = None
-        self.ultimo = None
 
     def agregar_articulo(self, articulo):
         nuevo_nodo = Nodo(articulo)
         if not self.primero:
             self.primero = nuevo_nodo
-            self.ultimo = nuevo_nodo
         else:
-            self.ultimo.siguiente = nuevo_nodo
-            nuevo_nodo.anterior = self.ultimo
-            self.ultimo = nuevo_nodo
+            actual = self.primero
+            while actual.siguiente:
+                actual = actual.siguiente
+            actual.siguiente = nuevo_nodo
+            nuevo_nodo.anterior = actual
 
     def mostrar_articulos(self):
         actual = self.primero
         while actual:
             print(f"articulo: {actual.dato}")
             actual = actual.siguiente
+    #Clase basica para mostrar que es doble:
+    def mostrar_articulos_reversa(self):
+        actual = self.primero
+        if not actual:
+            print("No hay articulos en el inventario.")
+            return
+        while actual.siguiente:
+            actual = actual.siguiente
+        while actual:
+            print(f"articulo: {actual.dato}")
+            actual = actual.anterior
 
 #aqui tambien se hacen las busquedas por lo que podemos usar la recursividad para encotrar el articulo
