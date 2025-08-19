b982cc228ba3bc8cc69cbe5ca1d2ef4bf06de0e7
Waloe <54786573+Eibon-bot@users.noreply.github.com>
2025-08-18 19:28:18 -0600

    Trabajar sobre Tarjetas, gestion de tarjetas y su relacion con el usuario

--- Tarjeta.py
+++ Tarjeta.py
@@ -1,9 +1,10 @@
 #Una composicion de las tarjetas con el usuario
 class TarjetaDeCompra:
-    def __init__(self, numero_tarjeta, codigo, banco):
+    def __init__(self, numero_tarjeta, codigo, banco, id_usuario):
         self.numero_tarjeta = numero_tarjeta
         self.codigo = codigo
         self.banco = banco
+        self.id_usuario = id_usuario
 
     def info_tarjeta(self):
         print(f"Tarjeta: {self.numero_tarjeta}")
